ed6c2e9eaa28e8ce8cad9a73e6662c2ce6dd9ee0
FedorUkrainets <123262701+FedorUkrainets@users.noreply.github.com>
2023-03-01 20:13:22 +0300

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,15 @@
+let secondPosition = 'burger'
+let thirdPosition = 'cola'
+
+
+const menu = {
+    firstPositoin: 'potato frise',
+    secondPosition,
+    thirdPosition,
+    fourthPosition: 'chiken burger'
+}
+
+menu.thirdPosition = 'pepsi cola'
+menu.fourthPosition = 'cheese burger'
+
+console.log(menu)
